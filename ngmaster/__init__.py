83a1c408363366340fab0d752ade55fc8c7b0f3c
Jason Kwong <kwongj@gmail.com>
2016-05-11 17:55:02 +1000

    Keep backup copies of db

--- ngmaster/__init__.py
+++ ngmaster/__init__.py
@@ -17,7 +17,6 @@ from urllib2 import urlopen, HTTPError, URLError
 import subprocess
 import shutil
 import re
-import tempfile
 from sys import argv
 from subprocess import Popen
 from Bio import SeqIO
@@ -35,27 +34,6 @@ def err(*args, **kwargs):
 	msg(*args, **kwargs)
 	sys.exit(1);
 
-# Perform the pure-Python equivalent of in-place `sed` substitution: e.g.,
-# `sed -i -e 's/'${pattern}'/'${repl}' "${filename}"`.
-def sed_inplace(filename, pattern, repl):
-	pattern_compiled = re.compile(pattern)
-	with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
-		with open(filename) as src_file:
-			for line in src_file:
-				tmp_file.write(pattern_compiled.sub(repl, line))
-	shutil.copystat(filename, tmp_file.name)
-	shutil.move(tmp_file.name, filename)
-
-# sed with . matching all characters including newline
-def sed_all(filename, pattern, repl):
-	pattern_compiled = re.compile(pattern, re.DOTALL)
-	with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
-		with open(filename, 'r') as src_file:
-			d = src_file.read()
-			tmp_file.write(pattern_compiled.sub(repl, d))
-	shutil.copystat(filename, tmp_file.name)
-	shutil.move(tmp_file.name, filename)
-
 # Format database
 def format(file):
 	sed_all(file, '^.*<textarea name="concatenation".*?>', '')
@@ -71,7 +49,7 @@ def fasta_to_dict(db):
 		dict[str(allele.seq.reverse_complement())] = (allele.id)
 	return dict
 
-#update DB function
+# Update DB function
 def update_db(db_folder, db_file, db_url, allele_db = False):
 	'''
 	A function to update the database
@@ -86,35 +64,40 @@ def update_db(db_folder, db_file, db_url, allele_db = False):
 		if not os.path.exists(db_folder):
 		    os.makedirs(db_folder)
 	except:
-		err( "Could not find/create db folder:'{}'".format( db_folder) )
+		err("Could not find/create db folder:'{}'".format( db_folder))
 	#download and process db information
 	try:
+		if os.path.isfile(db_file):
+			os.rename(db_file, db_file+'.old')
 		page = requests.get(db_url)
 		if not allele_db:
 			# all alleles are in a single tag <textarea>, and lines are
 			# separated by '\r\n'
-			#the first line will decode the tag area, splitting on the
+			# the first line will decode the tag area, splitting on the
 			# separator, creating a list'
 			# the second line joins the elements of the list to create
 			# a single string for saving to file.
-			soup = BeautifulSoup( page.text, "html.parser" )
-			new_db = soup.select( "textarea" )[0].text.split( "\r\n" )
-			new_db = '\n'.join(new_db[0:-1]) # the -1 is to remove the last
-											# empty field
+			soup = BeautifulSoup(page.text, "html.parser")
+			new_db = soup.select("textarea")[0].text.split("\r\n")
+			new_db = '\n'.join(new_db[0:-1]) 
+			# the -1 is to remove the last empty field
 		else:
-			#page is just a comma separated text file, with <br> tags
-			#for newlines
-			#need to translate the </br> tags to newline character, and then
-			#the text comes out ready to save to file
-			new_db = page.text.split( "<br>" )
-			new_db = '\n'.join( new_db )
+			# page is just a comma separated text file, with <br> tags
+			# for newlines
+			# need to translate the </br> tags to newline character, and then
+			# the text comes out ready to save to file
+			new_db = page.text.split("<br>")
+			new_db = '\n'.join(new_db)
 	except:
-		err("Unable to download/process URL:'{}'".format( db_url ) )
+		err("Unable to download/process URL:'{}'".format(db_url))
 	# save new db to db_file
 	try:
-		f = open( db_file, 'w' )
-		f.write( new_db )
+		f = open(db_file,'w')
+		if not allele_db:
+			f.write(new_db)
+		else:
+			f.write('ST'+','+'POR'+','+'TBPB'+'\n'+new_db)
 		f.close()
 	except:
-		err( "Could not save db file: '{}'".format( db_file ) )
+		err("Could not save db file: '{}'".format(db_file))
 	msg(db_file + ' ... Done.')
