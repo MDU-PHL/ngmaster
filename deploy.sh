9590f7ea542bb2d9248721697e8a4869ee79c946
Anders Goncalves da Silva <andersgs@gmail.com>
2020-05-04 17:14:12 -0700

    Fix typo in script

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo "# Before you deploy, have you:"
 echo "# 1. Checked that the version number has been bumped, and commited?"
 echo "# 2. Checked that the version of software in the README is correct, and commited any changes?"
 echo "# If so, run bash deploy.sh | bash"
-version=$(cat lissero/__init__.py | grep version | cut -f2 -d "=" | sed -E 's/^[[:space:]]+"(.*)"/v\1/g')
+version=$(cat ngmaster/__init__.py | grep version | cut -f2 -d "=" | sed -E 's/^[[:space:]]+"(.*)"/v\1/g')
 echo "git tag -a $version"
 echo "rm -rf build/* dist/*"
 echo "python3 setup.py sdist bdist_wheel"
