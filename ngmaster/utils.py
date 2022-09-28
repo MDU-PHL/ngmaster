26c76959fea986309a098b61ce986bdc22e6a943
Andreas Stroehlein <astroehlein@unimelb.edu.au>
2022-09-28 16:35:15 +1000

    Make update_db() work with PubMLST REST API data

--- ngmaster/utils.py
+++ ngmaster/utils.py
@@ -57,8 +57,17 @@ def update_db(db_folder, db_file, db_url, allele_db = False):
     try:
         if os.path.isfile(db_file):
             shutil.copy(db_file, db_file+'.old')
-        page = requests.get(db_url)
+        page = requests.get(db_url).text
         if not allele_db:
+            
+            #Change PubMLST format to ngmaster format
+            page = page.replace('>NG-MAST_','>')
+            page = page.replace('_','')
+            page = page.upper()
+            page = page.replace('PORB','POR')
+            
+            '''
+            AJS: old www.ng-mast.net code, remove after testing PubMLST implementation
             # all alleles are in a single tag <textarea>, and lines are
             # separated by '\r\n'
             # the first line will decode the tag area, splitting on the
@@ -69,22 +78,27 @@ def update_db(db_folder, db_file, db_url, allele_db = False):
             new_db = soup.select("textarea")[0].text.split("\r\n")
             new_db = '\n'.join(new_db[0:-1]) 
             # the -1 is to remove the last empty field
+            '''
         else:
+            page = page.replace('NG-MAST_','')
+            page = page.upper()
+            page = page.replace('\t',',')
+
+            '''
+            AJS: old www.ng-mast.net code, remove after testing PubMLST implementation
             # page is just a comma separated text file, with <br> tags
             # for newlines
             # need to translate the </br> tags to newline character, and then
             # the text comes out ready to save to file
             new_db = page.text.split("<br>")
             new_db = '\n'.join(new_db)
+            '''
     except:
         err("Unable to download/process URL:'{}'".format(db_url))
     # save new db to db_file
     try:
         f = open(db_file,'w')
-        if not allele_db:
-            f.write(new_db)
-        else:
-            f.write('ST'+','+'POR'+','+'TBPB'+'\n'+new_db)
+        f.write(new_db)
         f.close()
     except:
         err("Could not save db file: '{}'".format(db_file))
